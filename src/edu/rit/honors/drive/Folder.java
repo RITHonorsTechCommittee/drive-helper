ffc42cbec484141d4056d80daa0bf72914b0b03b
regdoug <pierce.reginald@gmail.com>
2014-02-12 21:57:33 -0500

    Wrote interface Folder.
    This interface is subject to change pending code review.

---
+++ src/edu/rit/honors/drive/Folder.java
@@ -0,0 +1,37 @@
+/* I wrote this in VIM! */
+
+import java.util.Collection;
+
+/**
+ * Extends the <code>File</code> interface with child operations
+ */
+
+interface Folder extends edu.rit.honors.drive.File {
+
+	/**
+	 * Optional.  Implementation will assist compatibility with
+	 * com.vaadin.data.util.Container.Hierarchical
+	 */
+	public boolean hasChildren();
+
+	/**
+	 * Return the direct children of this folder.  Note that this
+	 * will not traverse into subfolders. If you want to get all
+	 * children (including those in subfolders), use 
+	 * <code>getChildrenRecursive</code>.
+	 *
+	 * @return a <code>Collection</code> of children. The order 
+	 * in which the children are returned is not guaranteed.
+	 */
+	public Collection<File> getChildren();
+
+	/**
+	 * Return all the children of this folder, including children of
+	 * any subfolders.
+	 *
+	 * @return a <code>Collection</code> of children.  The order
+	 * in which the children are returned is not guaranteed.
+	 */
+	public Collection<File> getChildrenRecursive();
+
+}
