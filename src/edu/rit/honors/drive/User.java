70058ab4e246018bfdf3578b45c86d9bb885ee75
Ryan Rule-Hoffman <ry60003333@mac.com>
2014-02-18 16:11:32 -0500

    Changed the User class to be an interface.

--- src/edu/rit/honors/drive/User.java
+++ src/edu/rit/honors/drive/User.java
@@ -4,64 +4,25 @@ package edu.rit.honors.drive;
  * A user.
  * @author ry60003333
  */
-public class User
+public interface User
 {
-    /**
-     * The ID of the user.
-     */
-    private final int id;
-    
-    /**
-     * The name of the user.
-     */
-    private final String name;
-    
-    /**
-     * The email address of the user.
-     */
-    private final String email;
-    
-    /**
-     * Creates a new User.
-     * @param id The ID of the user.
-     * @param name The name of the user.
-     * @param email The email address of the user.
-     */
-    public User(
-            int id,
-            String name, 
-            String email
-    )
-    {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 
     /**
      * Get the ID of the user.
      * @return The ID.
      */
-    public int getId()
-    {
-        return id;
-    }
+    public int getId();
 
     /**
      * Get the name of the user.
      * @return The name.
      */
-    public String getName()
-    {
-        return name;
-    }
+    public String getName();
 
     /**
      * Get the email address of the user.
      * @return The email address.
      */
-    public String getEmail()
-    {
-        return email;
-    }
+    public String getEmail();
+    
 }
