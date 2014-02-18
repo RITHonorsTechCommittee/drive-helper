8daad2e6b8143e9784b5647842241164fa90d26e
Greg Daneault <gregdaneault@gmail.com>
2014-02-18 13:57:12 -0500

    File Interface updates
    I added some more to the File interface.  In doing so, I found that a
    lot of what we want to do with this interface already exists in

--- src/edu/rit/honors/drive/File.java
+++ src/edu/rit/honors/drive/File.java
@@ -5,6 +5,12 @@ import com.google.api.services.drive.model.File;
 /**
  * An (as of now incomplete) Implementation of the file interface for use in 
  * the Google Drive Helper program.
+ * 
+ * TODO:  DO WE NEED THIS?
+ * https://developers.google.com/resources/api-libraries/documentation/drive/v2/java/latest/com/google/api/services/drive/model/File.html
+ * 
+ * Also, https://developers.google.com/resources/api-libraries/documentation/drive/v2/java/latest/com/google/api/services/drive/model/User.html
+ * We will need to look at the entire Model package and see how much we want to wrap ourselves.
  */
 public interface File {
 	
@@ -12,6 +18,11 @@ public interface File {
 	 * Gets the primary owner of a the given file object.
 	 */
 	public User getOwner();
+	
+	/**
+	 * Gets a list of all the users who have full edit permissions
+	 */
+	public Collection<User> getEditors();
 	
 	/**
 	 * Attempts to change the ownership of a file in Google Drive.  If the 
@@ -31,17 +42,36 @@ public interface File {
 	 * 
 	 * TODO: Do we actually want to provide this, or should we completely wrap 
 	 * it and only expose what we deem "necessary" at the cost or redundency? 
+	 * 
+	 * This class also exposes a copy class which would be VERY helpful for 
+	 * hostile takeovers.  All we need to do is copy, delete the original, and 
+	 * rename as appropriate
 	 */
 	public com.google.api.services.drive.model.File getGoogleFile();
 	
 	
 	/**
 	 * Fetches the download URL for a given file resource.  This can be used 
-	 * later for the hostile takeover
+	 * later for the hostile takeover, or for archival purposes
 	 * 
 	 * @see https://developers.google.com/drive/web/manage-downloads
 	 */
 	public String downloadUrl(String mimeType);
+	
+	/**
+	 * Gets the name of the file
+	 */
+	public String getName();
+	
+	/**
+	 * Gets the MIME Type of a file
+	 */
+	public String getMIME();
+	
+	/**
+	 * Gets the parent of a file
+	 */
+	public Folder getParent();
 	
 	
 	
