94b011a1806a88d49114c75e6cb40a26e17a3f66
jmi4099 <jmi4099@rit.edu>
2014-03-04 16:04:00 -0500

    Updated UserHelper.java

--- src/edu/rit/honors/drive/UserHelper.java
+++ src/edu/rit/honors/drive/UserHelper.java
@@ -1,4 +1,6 @@
-
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.User;
+package edu.rit.honors.drive;
 public interface UserHelper {
 	
 	//Returns the User with the given email address
@@ -6,4 +8,8 @@ public interface UserHelper {
 
 	//Return the permissions of the given user
 	public Object getPermission(User u);
+
+	//Preconditions: User and file are valid
+	//Postcondition: Returns constants
+	public int getRole(User u, File f);
 }
